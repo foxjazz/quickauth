674c3ffd2a5b904129011272a20cf33aa0e7202d
foxjazz <foxr4@foxjazz.net>
2016-12-30 22:31:48 -0700

    added 3 calls for couchdb

--- backendNode/anonymous-routes.js
+++ backendNode/anonymous-routes.js
@@ -11,29 +11,15 @@ app.get('/api/random-quote', function(req, res) {
     res.status(200).send(quoter.getRandomOne());
 });
 
+app.get('/couchDataAll', function(req, res) {
+    let test = db.all();
+    res.status(200).send(db.all());
+});
+app.get('/couchGet', function(req, res) {
 
-app.post('/couchproxy', function(req, res) {
-
-    // console.log("todo:", req.body);
-
-    // res.status(200).json(todo);
-    // res.send('OK');
-    // if (!req.body.login || !req.body.password) {
-    //     return res.status(400).send("You must send the username and the password");
-    // }
-    // try {
-    //     isuser = jdb.getData("/" + req.body.login);
-    //     if (isuser.password === req.body.password) {
-    //         console.log('good pw on signin returning 201');
-    //         return res.status(201).send(iuser);
-    //     }
-
-    // } catch (error) {
-    //     console.log(error);
-    // }
-
-    // return res.status(401).send("The username or password don't match");
-
+    res.status(200).send(db.cache.get(req.query.id));
+});
+app.post('/couchSave', function(req, res) {
 
     db.save(req.body._id, req.body.couchbody, function(err, res) {
         if (err)
