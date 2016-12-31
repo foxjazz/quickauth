8c5aeae5c46933f3fd4a84368c1c5be031c5f1db
foxjazz <foxr4@foxjazz.net>
2016-12-31 00:26:36 -0700

    updated for couch

--- backendNode/anonymous-routes.js
+++ backendNode/anonymous-routes.js
@@ -12,12 +12,24 @@ app.get('/api/random-quote', function(req, res) {
 });
 
 app.get('/couchDataAll', function(req, res) {
-    let test = db.all();
-    res.status(200).send(db.all());
+    var members = [];
+    // let info = db.info();
+    // let dbs = db.databases();
+    var test = db.all(function(re, rs) {
+        var gots = JSON.parse(rs);
+        for (i = 0; i < gots.length; i++) {
+            db.get(gots[i].id, function(err, doc) {
+                members.push(doc);
+            })
+        }
+        res.status(200).send(JSON.stringify(members));
+    })
+
 });
 app.get('/couchGet', function(req, res) {
-
-    res.status(200).send(db.cache.get(req.query.id));
+    db.get(req.query.id, function(err,doc){
+        res.status(200).send(doc);
+    });
 });
 app.post('/couchSave', function(req, res) {
 
